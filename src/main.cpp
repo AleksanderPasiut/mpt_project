36f97f29ca5a20b753989c7cdbf0ae8a8bf33cd7
Aleksander Pasiut <apasiut@protonmail.com>
2023-10-27 23:51:08 +0200

    add port configurability

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,25 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
+#include <string>
+
 #include "server/server.file_application.hpp"
 
-int main()
+static std::string process_args(int argc, char* argv[])
 {
-    ServerFileApplication fsapp { "3490", "../website" };
+    if (argc == 2)
+    {
+        return std::string( argv[1] );
+    }
+    
+    return std::string("3490");
+}
+
+int main(int argc, char* argv[])
+{
+    const std::string port = process_args(argc, argv);
+
+    ServerFileApplication fsapp { port, "../website" };
 
     fsapp.set_default_path("index.html");
     fsapp.run();

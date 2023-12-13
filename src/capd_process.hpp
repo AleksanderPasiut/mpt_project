b0b1f71367ae490dfcaa5a02908682c271c7bcc9
Aleksander Pasiut <apasiut@protonmail.com>
2023-12-13 10:16:49 +0100

    split capd process hpp file

--- src/capd_process.hpp
+++ src/capd_process.hpp
@@ -6,51 +6,7 @@
 
 #include "process.hpp"
 
-#include <sstream>
-
-class CapdProcessParams
-{
-public:
-    CapdProcessParams(
-        const std::string& func_str,
-        const std::string& initial_condition_values_str,
-        const std::string& integration_time_str,
-        unsigned order,
-        unsigned decimal_places)
-    {
-        std::stringstream ss {};
-        ss << "backend/capd_backend ";
-        ss << process_str_param(func_str, "formula string")  << ' ';
-        ss << process_str_param(initial_condition_values_str, "initial condition values string")  << ' ';
-        ss << process_str_param(integration_time_str, "integration time string")  << ' ';
-        ss << order << ' ';
-        ss << decimal_places;
-
-        m_cmd = ss.str();
-    }
-
-    const std::string& get_cmd() const noexcept
-    {
-        return m_cmd;
-    }
-
-private:
-    static std::string process_str_param(
-        const std::string& arg,
-        const std::string& param)
-    {
-        if( arg.find_first_of('\"') != std::string::npos)
-        {
-            std::stringstream ss {};
-            ss << "Invalid characters in " << param;
-            throw std::logic_error(ss.str());
-        }
-
-        return '\"' + arg + '\"';
-    }
-
-    std::string m_cmd;
-};
+#include "capd_process.params.hpp"
 
 class CapdProcess
 {

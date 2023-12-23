27721a64b25ec526cfedb493e489e7e155a21449
Aleksander Pasiut <apasiut@protonmail.com>
2023-12-23 11:05:23 +0100

    multiclient support stub

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,69 @@ static std::string process_args(int argc, char* argv[])
     return std::string("3490");
 }
 
+class SessionIdGenerator
+{
+public:
+    std::string get()
+    {
+        std::stringstream ss {};
+        ss << "SessionIdDummy_" << m_counter;
+        ++m_counter;
+        return ss.str();
+    }
+
+private:
+    size_t m_counter {};
+};
+
+class ParametersBufferMulticlient
+{
+public:
+    using Func = CustomResponse(ParametersBuffer::*)(const std::string_view& query, const std::string_view& cookie);
+
+    ParametersBufferMulticlient()
+    {}
+
+    std::string initialize()
+    {
+        std::string const cookie_data = m_session_id_generator.get();
+
+        m_buffer[cookie_data] = ParametersBuffer();
+
+        return cookie_data;
+    }
+
+    CustomResponse invoke_buffer_function(const std::string_view& query, const std::string_view& cookie, Func func)
+    {
+        const std::string session_id = std::string(cookie);
+
+        auto it = m_buffer.find(session_id);
+        if (it != m_buffer.end())
+        {
+            std::cout << __func__ << " " << session_id << '\n';
+            
+            ParametersBuffer& parameters_buffer = it->second;
+            return std::invoke(func, &parameters_buffer, query, cookie);
+        }
+        else
+        {
+            std::cerr << "Unknown session id! " << session_id << "\n";
+            return CustomResponse(500);
+        }
+    }
+
+private:
+    SessionIdGenerator m_session_id_generator {};
+
+    std::unordered_map<std::string, ParametersBuffer> m_buffer {};
+};
+
 int main(int argc, char* argv[])
 {
     const std::string port = process_args(argc, argv);
 
-    ParametersBuffer m_params {};
+    ParametersBufferMulticlient m_parameters_buffer_multiclient {};
+    // ParametersBuffer m_params {};
 
     generate_qr_code_bmp(port);
 
@@ -32,34 +90,75 @@ int main(int argc, char* argv[])
 
     fsapp.get_impl().set_default_path("index.html");
 
-    fsapp.get_impl().register_on_post_handler("/initialize", [](const std::string_view& contents, const std::string_view& cookie) -> CustomResponse
+    fsapp.get_impl().register_custom_handler("/qr_code.bmp", get_qr_code);
+
+
+    fsapp.get_impl().register_on_post_handler("/initialize", [&m_parameters_buffer_multiclient](const std::string_view& contents, const std::string_view& cookie) -> CustomResponse
     {
         std::cout << "initialize\n";
-        return CustomResponse(200, "text/plain", "", "DummyCookieValue");
+        std::string const session_id = m_parameters_buffer_multiclient.initialize();
+        return CustomResponse(200, "text/plain", "", session_id);
     });
 
-    fsapp.get_impl().register_custom_handler("/values.txt",
-        std::bind(&ParametersBuffer::handle_parameters_get, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    auto register_custom_handler_helper = [&fsapp, &m_parameters_buffer_multiclient](std::string&& path, ParametersBufferMulticlient::Func func)
+    {
+        fsapp.get_impl().register_custom_handler(path,
+            std::bind(&ParametersBufferMulticlient::invoke_buffer_function, &m_parameters_buffer_multiclient, std::placeholders::_1, std::placeholders::_2, func));
+    };
 
-    fsapp.get_impl().register_custom_handler("/value",
-        std::bind(&ParametersBuffer::handle_parameter_set, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    auto register_on_post_handler_helper = [&fsapp, &m_parameters_buffer_multiclient](std::string&& path, ParametersBufferMulticlient::Func func)
+    {
+        fsapp.get_impl().register_on_post_handler(path,
+            std::bind(&ParametersBufferMulticlient::invoke_buffer_function, &m_parameters_buffer_multiclient, std::placeholders::_1, std::placeholders::_2, func));
+    };
 
-    fsapp.get_impl().register_custom_handler("/qr_code.bmp", get_qr_code);
+    register_custom_handler_helper("/values.txt",
+        &ParametersBuffer::handle_parameters_get);
+
+    register_custom_handler_helper("/value",
+        &ParametersBuffer::handle_parameter_set);
+
+    register_custom_handler_helper("/string.txt",
+        &ParametersBuffer::get_string_output);
+        
+    register_on_post_handler_helper("/string0",
+        &ParametersBuffer::set_string<0>);
+
+    register_on_post_handler_helper("/string1",
+        &ParametersBuffer::set_string<1>);
     
-    fsapp.get_impl().register_on_post_handler("/string0",
-        std::bind(&ParametersBuffer::set_string<0>, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    register_on_post_handler_helper("/string2",
+        &ParametersBuffer::set_string<2>);
     
-    fsapp.get_impl().register_on_post_handler("/string1",
-        std::bind(&ParametersBuffer::set_string<1>, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    register_on_post_handler_helper("/triggers",
+        &ParametersBuffer::compute);
+
+
+    // fsapp.get_impl().register_custom_handler("/values.txt",
+    //     std::bind(&ParametersBufferMulticlient::invoke_buffer_function, &m_parameters_buffer_multiclient, std::placeholders::_1, std::placeholders::_2, &ParametersBuffer::handle_parameters_get));
+
+
+    // fsapp.get_impl().register_custom_handler("/values.txt",
+    //     std::bind(&ParametersBuffer::handle_parameters_get, &m_params, std::placeholders::_1, std::placeholders::_2) );
+
+    // fsapp.get_impl().register_custom_handler("/value",
+    //     std::bind(&ParametersBuffer::handle_parameter_set, &m_params, std::placeholders::_1, std::placeholders::_2) );
+
     
-    fsapp.get_impl().register_on_post_handler("/string2",
-        std::bind(&ParametersBuffer::set_string<2>, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    // fsapp.get_impl().register_on_post_handler("/string0",
+    //     std::bind(&ParametersBuffer::set_string<0>, &m_params, std::placeholders::_1, std::placeholders::_2) );
     
-    fsapp.get_impl().register_on_post_handler("/trigger",
-        std::bind(&ParametersBuffer::compute, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    // fsapp.get_impl().register_on_post_handler("/string1",
+    //     std::bind(&ParametersBuffer::set_string<1>, &m_params, std::placeholders::_1, std::placeholders::_2) );
     
-    fsapp.get_impl().register_custom_handler("/string.txt",
-        std::bind(&ParametersBuffer::get_string_output, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    // fsapp.get_impl().register_on_post_handler("/string2",
+    //     std::bind(&ParametersBuffer::set_string<2>, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    
+    // fsapp.get_impl().register_on_post_handler("/trigger",
+    //     std::bind(&ParametersBuffer::compute, &m_params, std::placeholders::_1, std::placeholders::_2) );
+    
+    // fsapp.get_impl().register_custom_handler("/string.txt",
+    //     std::bind(&ParametersBuffer::get_string_output, &m_params, std::placeholders::_1, std::placeholders::_2) );
 
     fsapp.run();
 

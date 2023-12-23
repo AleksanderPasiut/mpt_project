9d74b9ce21f112dd8b4c43b1358c16922bc200f7
Aleksander Pasiut <apasiut@protonmail.com>
2023-12-23 12:38:02 +0100

    minor layout cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -20,26 +20,15 @@ static std::string process_args(int argc, char* argv[])
     return std::string("3490");
 }
 
-int main(int argc, char* argv[])
+static void initialize_session_manager(ServerFileApplication& fsapp, SessionManager& session_manager)
 {
-    const std::string port = process_args(argc, argv);
-
-    SessionManager session_manager {};
-
-    generate_qr_code_bmp(port);
-
-    ServerFileApplication fsapp { port, "../website" };
-
-    fsapp.get_impl().set_default_path("index.html");
-
-    fsapp.get_impl().register_custom_handler("/qr_code.bmp", get_qr_code);
-
-    fsapp.get_impl().register_on_post_handler("/initialize", [&session_manager](const std::string_view& contents, const std::string_view& cookie) -> CustomResponse
-    {
-        std::cout << "initialize\n";
-        std::string const session_id = session_manager.initialize();
-        return CustomResponse(200, "text/plain", "", session_id);
-    });
+    fsapp.get_impl().register_on_post_handler("/initialize",
+        [&session_manager](const std::string_view& contents, const std::string_view& cookie) -> CustomResponse
+        {
+            std::cout << "initialize\n";
+            std::string const session_id = session_manager.initialize();
+            return CustomResponse(200, "text/plain", "", session_id);
+        });
 
     auto register_custom_handler_helper = [&fsapp, &session_manager](std::string&& path, SessionManager::Func func)
     {
@@ -73,6 +62,19 @@ int main(int argc, char* argv[])
     
     register_on_post_handler_helper("/trigger",
         &Session::compute);
+}
+
+int main(int argc, char* argv[])
+{
+    const std::string port = process_args(argc, argv);
+    generate_qr_code_bmp(port);
+
+    ServerFileApplication fsapp { port, "../website" };
+    fsapp.get_impl().set_default_path("index.html");
+    fsapp.get_impl().register_custom_handler("/qr_code.bmp", get_qr_code);
+
+    SessionManager session_manager {};
+    initialize_session_manager(fsapp, session_manager);
 
     fsapp.run();
 
